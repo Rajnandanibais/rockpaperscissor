0c4b5a87bc090468cb6756988b353b561883a149
KishoreBabu420 <kishore.sai37@gmail.com>
2021-05-14 16:28:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,104 +1,98 @@
 'use strict';
 
-const choices=['paper','scissor','rock'];
-const button=document.querySelectorAll('.pick');
-const scoreEl=document.getElementById('score');
-const main=document.getElementById('main');
-const selection=document.getElementById('selection');
-const reset=document.getElementById('reset');
-const userSelect=document.getElementById('userSelect');
-const computerSelect=document.getElementById('computerSelect');
-const userImg=document.querySelector('.user-img');
-const computerImg=document.querySelector('.computer-img');
-const winner=document.getElementById('winner');
+const choices = ['paper', 'scissors', 'rock'];
+const button = document.querySelectorAll('.pick');
+const scoreEl = document.getElementById('score');
+const main = document.getElementById('main');
+const selection = document.getElementById('selection');
+const reset = document.getElementById('reset');
+const userSelect = document.getElementById('userSelect');
+const computerSelect = document.getElementById('computerSelect');
+const userImg = document.querySelector('.user-img');
+const computerImg = document.querySelector('.computer-img');
+const winner = document.getElementById('winner');
 //rule modal
-const openbtn=document.getElementById('open');
-const closebtn=document.getElementById('close');
-const modal=document.getElementById('modal');
-
+const openbtn = document.getElementById('open');
+const closebtn = document.getElementById('close');
+const modal = document.getElementById('modal');
 
 //console.log(pickRandomChoice());
-let score=0;
-let userchoice=undefined;
-let computerchoice=undefined;
+let score = 0;
+let userchoice = undefined;
+let computerchoice = undefined;
 //function for userchoice
 
 button.forEach((button) => {
-button.addEventListener('click', function() {
- userchoice=button.getAttribute('data-choice');
- //console.log(userchoice);
+  button.addEventListener('click', function () {
+    userchoice = button.getAttribute('data-choice');
+    //console.log(userchoice);
 
-   checkWinner();
-   updateSelection();
-
-});
-});
-
-reset.addEventListener('click',()=>{
-//show main hide selection
-main.style.display='flex';
-selection.style.display='none';
-
-})
-
-openbtn.addEventListener('click',()=>{
-modal.style.display='flex';
+    checkWinner();
+    updateSelection();
+  });
 });
 
-closebtn.addEventListener('click',()=>{
-modal.style.display='none';
+reset.addEventListener('click', () => {
+  //show main hide selection
+  main.style.display = 'flex';
+  selection.style.display = 'none';
+});
+
+openbtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+closebtn.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
 
 function checkWinner() {
-let computerchoice=pickRandomChoice();
+  computerchoice = pickRandomChoice();
 
+  //updateSelection(userSelect,userchoice);
+  //updateSelection(computerSelect,computerchoice);
 
-//updateSelection(userSelect,userchoice);
-//updateSelection(computerSelect,computerchoice);
-
-if(userchoice===computerchoice){
-    winner.innerText='draw';
-    
-}else if((userchoice==='paper' && computerchoice==='rock') ||
-(userchoice==='rock'&&computerchoice==='scissor') ||
-(userchoice==='scissoe'&&computerchoice==='paper')){
+  if (userchoice === computerchoice) {
+    winner.innerText = 'draw';
+  } else if (
+    (userchoice === 'paper' && computerchoice === 'rock') ||
+    (userchoice === 'rock' && computerchoice === 'scissors') ||
+    (userchoice === 'scissors' && computerchoice === 'paper')
+  ) {
     //userwin
     updateScore(1);
-    winner.innerText='win';
-}else{
+    winner.innerText = 'win';
+  } else {
     //computerwin
     updateScore(-1);
-    winner.innerText='lost'
-}
-   //show selection hide main
-main.style.display='none';
-selection.style.display='flex';
-
-
+    winner.innerText = 'lost';
+  }
+  //show selection hide main
+  main.style.display = 'none';
+  selection.style.display = 'flex';
 }
 
-function updateScore(value){
-    score+=value;
-    scoreEl.innerText=score;
+function updateScore(value) {
+  score += value;
+  scoreEl.innerText = score;
 }
 
 function pickRandomChoice() {
-    const choices=['paper','rock','scissor'];
-    return choices[Math.trunc(Math.random()*choices.length)]
-    ;
+  const choices = ['paper', 'rock', 'scissors'];
+  return choices[Math.trunc(Math.random() * choices.length)];
 }
 
-function updateSelection(){
-userSelect.classList.remove("btn-paper");
-userSelect.classList.remove("btn-rock");
-userSelect.classList.remove("btn-scissor");
-computerSelect.classList.remove("btn-paper");
-computerSelect.classList.remove("btn-rock");
-computerSelect.classList.remove("btn-scissor");
+function updateSelection() {
+  userSelect.classList.remove('btn-paper');
+  userSelect.classList.remove('btn-rock');
+  userSelect.classList.remove('btn-scissors');
+  computerSelect.classList.remove('btn-paper');
+  computerSelect.classList.remove('btn-rock');
+  computerSelect.classList.remove('btn-scissors');
 
-userSelect.classList.add(`btn-${userchoice}`);
-computerSelect.classList.add(`btn-${computerchoice}`);
+  userSelect.classList.add(`btn-${userchoice}`);
+  computerSelect.classList.add(`btn-${computerchoice}`);
 
-userImg.src=` img/icon-${userchoice}.svg`;
-computerImg.src=`img/icon-${computerchoice}.svg`;
+  userImg.src = ` img/icon-${userchoice}.svg`;
+  computerImg.src = `img/icon-${computerchoice}.svg`;
 }
